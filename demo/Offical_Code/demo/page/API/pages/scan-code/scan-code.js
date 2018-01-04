4a96116df5ff2664e7c34a03ed1b799b5fa85bfd
昱霖君 <1946682413@qq.com>
2018-01-04 20:49:24 +0800

    upload demo

---
+++ demo/Offical_Code/demo/page/API/pages/scan-code/scan-code.js
@@ -0,0 +1,17 @@
+Page({
+  data: {
+    result: ''
+  },
+  scanCode: function () {
+    var that = this
+    wx.scanCode({
+      success: function (res) {
+        that.setData({
+          result: res.result
+        })
+      },
+      fail: function (res) {
+      }
+    })
+  }
+})
